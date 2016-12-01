e4a7bbfbabd5eddfba64150ea9b453ebf1a641ad
Andrey Nakin <andrey.nakin@gmail.com>
2016-12-01 19:37:11 +0300

    work

--- assembly007/assembly007.tcl
+++ assembly007/assembly007.tcl
@@ -87,6 +87,7 @@ proc stopMeasure {} {
     # \u0417\u0430\u043F\u0440\u0435\u0449\u0430\u0435\u043C \u043A\u043D\u043E\u043F\u043A\u0443 \u043E\u0441\u0442\u0430\u043D\u043E\u0432\u0430 \u0438\u0437\u043C\u0435\u0440\u0435\u043D\u0438\u0439    
 	$w.nb.m.ctl.stop configure -state disabled
 	$w.nb.m.ctl.measure configure -state disabled
+	$w.nb.m.ctl.suspend configure -state disabled
 	$w.nb.m.ctl.addComment configure -state disabled
 }
 
@@ -112,6 +113,7 @@ proc startMeasure {} {
     # \u0420\u0430\u0437\u0440\u0435\u0448\u0430\u0435\u043C \u043A\u043D\u043E\u043F\u043A\u0443 \u043E\u0441\u0442\u0430\u043D\u043E\u0432\u0430 \u0438\u0437\u043C\u0435\u0440\u0435\u043D\u0438\u0439
 	$w.nb.m.ctl.stop configure -state normal
 	$w.nb.m.ctl.measure configure -state normal
+	$w.nb.m.ctl.suspend configure -state normal
 	$w.nb.m.ctl.addComment configure -state normal
 	
     # \u041E\u0447\u0438\u0449\u0430\u0435\u043C \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u044B \u0432 \u043E\u043A\u043D\u0435 \u043F\u0440\u043E\u0433\u0440\u0430\u043C\u043C\u044B
@@ -128,6 +130,7 @@ proc terminateMeasure {} {
     # \u0417\u0430\u043F\u0440\u0435\u0449\u0430\u0435\u043C \u043A\u043D\u043E\u043F\u043A\u0443 \u043E\u0441\u0442\u0430\u043D\u043E\u0432\u0430 \u0438\u0437\u043C\u0435\u0440\u0435\u043D\u0438\u0439    
 	$w.nb.m.ctl.stop configure -state disabled
 	$w.nb.m.ctl.measure configure -state disabled
+	$w.nb.m.ctl.suspend configure -state disabled
 	$w.nb.m.ctl.addComment configure -state disabled
 	
 	# \u041F\u043E\u0441\u044B\u043B\u0430\u0435\u043C \u0432 \u0438\u0437\u043C\u0435\u0440\u0438\u0442\u0435\u043B\u044C\u043D\u044B\u0439 \u043F\u043E\u0442\u043E\u043A \u0441\u0438\u0433\u043D\u0430\u043B \u043E\u0431 \u043E\u0441\u0442\u0430\u043D\u043E\u0432\u0435
@@ -281,9 +284,9 @@ set p [ttk::labelframe $w.nb.m.ctl -text " \u0423\u043F\u0440\u0430\u0432\u043B\
 pack $p -fill x -side bottom -padx 10 -pady 5
 
 grid [ttk::button $p.measure -text "\u0421\u043D\u044F\u0442\u044C \u0442\u043E\u0447\u043A\u0443" -state disabled -command makeMeasurement -image ::img::next -compound left] -row 0 -column 0 -sticky w
-grid [ttk::checkbutton $p.suspend1 -image ::img::pause -compound left -style Toolbutton -state disabled -text "Приостановить запись"] -row 0 -column 1 -sticky w
+grid [ttk::checkbutton $p.suspend -image ::img::pause -compound left -style Toolbutton -state disabled -text "\u041F\u0440\u0438\u043E\u0441\u0442\u0430\u043D\u043E\u0432\u0438\u0442\u044C \u0437\u0430\u043F\u0438\u0441\u044C"] -row 0 -column 1 -sticky w
 grid [ttk::entry $p.comment -textvariable measureComment] -row 0 -column 2 -sticky we
-grid [ttk::button $p.addComment -text "\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u043A\u043E\u043C\u043C\u0435\u043D\u0442\u0430\u0440\u0438\u0439" -state disabled -command addComment -compound left] -row 0 -column 3 -sticky w
+grid [ttk::button $p.addComment -text "\u0414\u043E\u0431\u0430\u0432\u0438\u0442\u044C \u043A\u043E\u043C\u043C\u0435\u043D\u0442\u0430\u0440\u0438\u0439" -state disabled -command addComment -image ::img::edit -compound left] -row 0 -column 3 -sticky w
 grid [ttk::button $p.stop -text "\u041E\u0441\u0442\u0430\u043D\u043E\u0432\u0438\u0442\u044C \u0437\u0430\u043F\u0438\u0441\u044C" -command terminateMeasure -state disabled -image ::img::stop -compound left] -row 0 -column 4 -sticky e
 grid [ttk::button $p.start -text "\u041D\u0430\u0447\u0430\u0442\u044C \u0437\u0430\u043F\u0438\u0441\u044C" -command startMeasure -image ::img::start -compound left] -row 0 -column 5 -sticky e
 
