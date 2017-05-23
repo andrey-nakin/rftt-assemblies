4479495863bf1ad831b9650c4b6b673eb6395515
Andrey Nakin <andrey.nakin@gmail.com>
2017-05-23 16:10:45 +0300

    work

--- assembly007/build.cmd
+++ assembly007/build.cmd
@@ -1,7 +1,11 @@
+set APP_VERSION=2.2.0
+
 tclkitsh8513 sdx.kit wrap assembly007 -runtime tclkit-8.5.8-win32.upx.exe
 erase assembly007.exe
 rename assembly007 assembly007.exe
 cd doc
 pdflatex manual.tex
 cd ..
-copy /Y doc\manual.pdf manual.pdf
+
+7z a assembly007-%APP_VERSION%-win32.zip assembly007.exe
+7z a assembly007-%APP_VERSION%-win32.zip doc/manual.pdf
